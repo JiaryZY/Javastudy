19713ba8c6afaec964c72f64cbc7f1d1c0e6c2a8
JiaryZY <108994968+JiaryZY@users.noreply.github.com>
2022-10-11 13:08:44 +0100

    1
    1

--- homework1/java/src/com/solitaire/view/MainMenu.java
+++ homework1/java/src/com/solitaire/view/MainMenu.java
@@ -8,16 +8,16 @@ public class MainMenu {
 		System.out.println("\033[34;1mS.Start Game\033[0m");
 		System.out.println("\033[34;1mQ.Quit\033[0m");
 
-		char selection = new java.util.Scanner(System.in).next().charAt(0); // 锁定字节长度
+		char selection = new java.util.Scanner(System.in).next().charAt(0); // Lock byte length
 
 		System.out.println("Your input is " + selection);
-		if (selection == 'S' && selection == 's')// 大小写适配
+		if (selection == 'S' || selection == 's')// Upper and lower case adaptation
 		{
 			System.out.println("Game Start");
 			GamePlay.start();
 		}
 
-		else if (selection == 'Q' && selection == 'q') {
+		else if (selection == 'Q' || selection == 'q') {
 			System.out.println("You have exited the game");
 			System.out.println("Welcome back");
 			System.exit(0);
@@ -25,14 +25,14 @@ public class MainMenu {
 
 		else {
 			
-			for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 20; i++)
 				System.out.println(" ");    // clear
 			
-			System.out.println("————————————————————————————————————————————————————");
+			//System.out.println("————————————————————————————————————————————————————");
 			System.out.println("\033[31;1minvalid command! please enter the correct command!!\033[0m");
 
 			//System.out.println("——————————————————————————————————————————————————");
-			System.out.println("————————————————————————————————————————————————————");
+			//System.out.println("————————————————————————————————————————————————————");
 
 			MainMenu.show();
 
